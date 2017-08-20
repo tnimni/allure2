20fa15c7f1219f9e80e11190c2a7dc142f997915
Tsachi Nimni <tsachinimni@Tsachis-MacBook-Pro.local>
2017-08-20 14:28:14 +0300

    first commit

---
+++ src/test/java/firstTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by tsachinimni on 17/08/2017.
+ */
+public class firstTest {
+
+    @Test
+    public void consoleLogger() {
+        System.out.println("debugging");
+        Assert.assertEquals(1, 1);
+    }
+}
